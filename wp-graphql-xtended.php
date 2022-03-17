3924a25774d4184f5398939ae5e8f889c338c30c
Cloudbop <boppincolin@gmail.com>
2022-03-17 21:47:28 +0000

    + new type to schema @ root of graph

--- wp-graphql-xtended.php
+++ wp-graphql-xtended.php
@@ -8,4 +8,28 @@
  * Description: This is an example plugin for extending WP-GraphQL
  */
 
+// 
+add_action('graphql_register_types', 'wpgql_extended_register_types');
+// 
+function wpgql_extended_register_types(){
+  // Register a new type in the Schema
+  register_graphql_field('RootQuery', 'example', [
+    //doc
+    'description' => __( 'This is just an example field', 'your-textdomain' ),
+    'type' => 'String',
+    // resolver 
+    'resolve' => function(
+      // root, prev object that was resolved
+      $root, 
+      // input graphql args
+      $args, 
+      // state of graphql resolver, current user executing request
+      $context, 
+      // object that provides state of tree/graph
+      $info
+    ) {
+      return "Hello.";
+    }
+  ]);
 
+}
