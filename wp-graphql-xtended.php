6badb5f02c8efe0816d459465c2268cb2986c166
Cloudbop <boppincolin@gmail.com>
2022-03-17 21:21:13 +0000

    bp wp plugin

---
+++ wp-graphql-xtended.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: WP GraphQL Xtended
+ * Plugin URI: https://www.wp-graphql-xtended.com
+ * Version: 0.0.1-alpha
+ * Author:
+ * Author URI:
+ * Description: This is an example plugin for extending WP-GraphQL
+ */
+
+
