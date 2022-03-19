6cb1d5d6ab5d89ca97f86047fb68bda03791e9a2
Cloudbop <boppincolin@gmail.com>
2022-03-19 01:05:14 +0000

    +new color field to Post

--- wp-graphql-xtended.php
+++ wp-graphql-xtended.php
@@ -36,7 +36,7 @@ function wpgql_extended_register_types(){
       $info
     ) {
 
-      // will resolve the function early and return json;
+      // will resolve the function and return json in wpgraphiql
       // wp_send_json( [ 'args' => $args ]);
       
       // use wpgrapql debug - requires debug to be enabled. -> sends message with resolver
@@ -46,4 +46,46 @@ function wpgql_extended_register_types(){
     }
   ]);
 
+  // Register a new type in the Schema at root of graph
+  register_graphql_field('Post', 'color', [
+    //doc
+    'description' => __( 'The color field meta', 'your-textdomain' ),
+    'type' => 'String',
+    // 'args' => [
+    //   'test'=> [
+    //     //doc
+    //     'description' => __( 'This is just an example field', 'your-textdomain' ),
+    //     'type' => "String"
+    //   ],
+    // ],
+    // resolver 
+    'resolve' => function(
+      // prev object that was resolved, (@ root of graph)
+      $post, 
+      // input graphql args
+      $args, 
+      // state of graphql resolver, current user executing request
+      $context, 
+      // object that provides state of tree/graph
+      $info
+    ) {
+      // will resolve the function early and return json;
+      // wp_send_json( [ 'args' => $args ]);
+      
+      // use wpgrapql debug - requires debug to be enabled. -> sends message with resolver
+      // graphql_debug( $post );
+      //... will still resolve
+
+      $color = get_post_meta($post->databaseId, 'color', true);
+      // nullable
+      return $color ?? null;
+      //what's this? wp_kses_no_null($string, $options);
+    }
+  ]);
 }
+
+// store color value as post meta data
+// add_action( 'init', function(){
+// double check this creates the meta on every post.
+// update_post_meta($post_id="3551", $meta_key="color", $meta_value="blue", $prev_value);
+// });
